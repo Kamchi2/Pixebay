5ddc9baddf5641eeda23805512c2062fcc3f1273
KamchiDjunusov <dimooooon70@gmail.com>
2022-09-23 21:36:32 +0200

    Initial commit

--- app/src/main/java/com/example/pixebay/PixeApi.kt
+++ app/src/main/java/com/example/pixebay/PixeApi.kt
@@ -7,8 +7,9 @@ import retrofit2.http.Query
 interface PixeApi {
     @GET("api/?key=30126344-41522a727c59915668240ed5a")
     fun getImages(
-        @Query("q") keyWord: String,
+        @Query("q") q: String,
         @Query("page") page: Int = 1,
-        @Query("perPage") perPage: Int = 10
+        @Query("per_page") perPage: Int = 10
     ): Call<PixabayModel>
+
 }
